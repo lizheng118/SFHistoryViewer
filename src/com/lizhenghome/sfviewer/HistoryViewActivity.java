f77cec7e562f967ef4be055460e8c09c724b43f5
Zheng Li <lizheng118@gmail.com>
2011-05-21 00:37:17 +0900

    new created

---
+++ src/com/lizhenghome/sfviewer/HistoryViewActivity.java
@@ -0,0 +1,24 @@
+package com.lizhenghome.sfviewer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.example.R;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: lizheng
+ * Date: 11/05/21
+ * Time: 0:34
+ * To change this template use File | Settings | File Templates.
+ */
+public class HistoryViewActivity extends Activity{
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+    }
+}
